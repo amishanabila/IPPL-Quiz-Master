d865973acdc9cddc387bf9dcfbd0132ca091bec2
Amisha Nabila Wiguna <11231008@student.itk.ac.id>
2025-12-23 22:12:16 +0800

    up

--- backend/server.js
+++ backend/server.js
@@ -143,12 +143,18 @@ process.on('unhandledRejection', (err) => {
     process.exit(1);
 });
 
-const PORT = process.env.PORT || 5000;
-const server = app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+// const PORT = process.env.PORT || 5000;
+// const server = 
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });
 
-server.on('error', (err) => {
-    console.error('Server error:', err);
-    process.exit(1);
+// server.on('error', (err) => {
+//     console.error('Server error:', err);
+//     process.exit(1);
+// });
+
+const port = process.env.PORT || 5000;
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server running on port ${port}`);
 });
